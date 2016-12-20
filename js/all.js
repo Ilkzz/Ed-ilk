c3b0aa4221ba959ac2713659162cd2145119d197
Ilker Sahin <ilkzz@hotmail.com>
2016-12-20 14:54:03 +0000

    Update
    Update

--- js/all.js
+++ js/all.js
@@ -42,3 +42,24 @@ $('.returnToTop').click(function() {
         scrollTop : 0                     
     }, 500);
 });
+
+/*global document:false, $:false */
+var txt = $('#comments'),
+    hiddenDiv = $(document.createElement('div')),
+    content = null;
+
+//txt.addClass('txtstuff');
+hiddenDiv.addClass('hiddendiv common');
+
+$('body').append(hiddenDiv);
+
+txt.on('keyup', function () {
+
+    content = $(this).val();
+
+    content = content.replace(/\n/g, '<br>');
+    hiddenDiv.html(content + '<br class="lbr">');
+
+    $(this).css('height', hiddenDiv.height());
+
+});
