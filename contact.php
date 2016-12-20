d206f3ad8673c46e654875e9ef0d8c70dd7d3908
EddyKay101 <edwardkay10@gmail.com>
2016-12-20 11:34:02 +0000

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,3 +1,56 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>Contact Us - Ed&Ilk</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="http://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Alegreya:700" rel="stylesheet" type="text/css" />
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous">
+    </script>
+    <script src="JS/all.js"></script>
+</head>
+<body>
+	<header class="navigation">
+ 		<a href="index.html" id="logolink">
+			<img id="logo" src="images/logo.svg"  alt="ed&ilk logo"/>
+		</a>       
+       	<nav class="group">
+			<a href="index.html">Home</a>
+			<a href="services.html">Services</a>
+			<a href="">About</a>
+			<a class="active" href="http://localhost/Ed-ilk-dev-build/contact.php">Contact</a>
+		</nav>    
+	</header>
+<body>
+	<div class="headerImageCon">
+		<div class="box">
+    		<h1 class="heading">Contact us now for enquires.</h1>
+    		<div class="headingUnderline"></div>
+      	</div>
+		<div class="imageOverlay"></div>
+		<img class="backgroundImg"  src="images/contactus.svg"  alt="Header Background"/>
+	</div>
+	<section>
+		<h2 class='contacth2'>Get in touch!</h2>
+		<form method='post' action='' id='contact-form'> 
+			<input type='text' name='firstname' required  placeholder='First Name' size = '30' /><label class='warning' id='firstname-warning'></label><br><br>
+			<input type='text' name='surname' placeholder='Surname' size = '30' required /> <label class='warning' id='surname-warning'></label><br><br>
+			<input type='text' name='email' placeholder='Email' size = '30' required  /> <label class='warning' id='email-warning'></label><br><br>
+			<textarea name='message' placeholder='Message' cols='30'></textarea><br><br>
+			<input type='submit' value='Submit' name='new-msg' />
+
+			<!--<p id='user-form-message'>$userFormMessage</p>-->
+			</form>
+		</section>
+    <footer>
+		<p>&#169; ed&#38;ilk 2016 - All rights reserved - <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">hello@edandilk.com</a></p>
+	</footer>
+    
+</body>
+</html>
+
 <?php
 	//is form submitted?
 	$createContact = isset($_POST['new-msg']);
@@ -36,67 +89,3 @@
 		$userFormMessage = "";
 	}
 ?>
-
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-	<title>Contact Us - Ed&Ilk</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="http://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Alegreya:700" rel="stylesheet" type="text/css" />
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous">
-    </script>
-</head>
-<body>
-	<header class="navigation">
- 		<a href="index.html" id="logolink">
-			<img id="logo" src="images/logo.svg"  alt="ed&ilk logo"/>
-		</a>       
-       	<nav class="group">
-			<a class="active" href="index.html">Home</a>
-			<a href="services.html">Services</a>
-			<a href="">About</a>
-			<a href="http://localhost:1234/Ed-ilk-dev-build/contact-us.php">Contact</a>
-		</nav>    
-	</header>
-<body>
-<header class="navigation">
- 		<a href="index.html" id="logolink">
-			<img id="logo" src="images/logo.svg"  alt="ed&ilk logo"/>
-		</a>       
-       	<nav class="group">
-			<a class="active" href="index.html">Home</a>
-			<a href="services.html">Services</a>
-			<a href="">About</a>
-			<a href="http://localhost:1234/Ed-ilk-dev-build/contact-us.php">Contact</a>
-		</nav>    
-	</header>
-	<div class="headerImageCon">
-		<div class="box">
-    		<h1 class="heading">We build websites that build your business.</h1>
-    		<div class="headingUnderline"></div>
-      	</div>
-		<div class="imageOverlay"></div>
-		<img class="backgroundImg"  src="images/background.jpg"  alt="Header Background"/>
-	</div>
-	<section>
-		<h2 class='contacth2'>Get in touch!</h2>
-		<form method='post' action='' id='contact-form'> 
-			<input type='text' name='firstname' required  placeholder='First Name' /><label class='warning' id='firstname-warning'></label> 
-			<input type='text' name='surname' placeholder='Surname' required /> <label class='warning' id='surname-warning'></label>
-			<input type='text' name='email' placeholder='Email' required  /> <label class='warning' id='email-warning'></label>
-			<textarea name='message' placeholder='Message'></textarea>
-			<input type='submit' value='Submit' name='new-msg' />
-
-			<!--<p id='user-form-message'>$userFormMessage</p>-->
-			</form>
-		</section>
-    <footer>
-		<p>&#169; ed&#38;ilk 2016 - All rights reserved - <a href="mailto:someone@example.com?Subject=Hello%20again" target="_top">hello@edandilk.com</a></p>
-	</footer>
-    
-    <script type="text/javascript" src="js/all.js"></script>
-</body>
-</html>
